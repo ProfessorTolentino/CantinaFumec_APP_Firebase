91eb7f26930eea5aa90cfa0c239c5e1b45c030d1
Tolentinos IT Solutions <vinicius@professortolentino.com>
2022-06-15 18:06:08 -0300

    Atualização Angular 14, ionic 6.1.9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -10,28 +10,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuth } from 'angularfire2/auth'
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth';
+import {
+  AngularFirestoreModule
+} from '@angular/fire/compat/firestore';
 
-import { firebaseConfig } from './firebaseConfig'
+import { firebaseConfig } from './firebaseConfig';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
+    BrowserModule,
+    IonicModule.forRoot(),
     AppRoutingModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(firebaseConfig.fire)
+    provideFirebaseApp(() => initializeApp(firebaseConfig.fire)),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence(),
   ],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    AngularFireAuth
+    { provide: FIREBASE_OPTIONS, useValue: firebaseConfig.fire },
+    AngularFireAuth,
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 import AuthProvider = firebase.auth.AuthProvider;
 
 @Injectable({
@@ -18,12 +18,12 @@ export class AuthService {
 
   signInWithEmail(credentials) {
     console.log('Sign in with email');
-    return this.afAuth.auth.signInWithEmailAndPassword(credentials.email,
+    return this.afAuth.signInWithEmailAndPassword(credentials.email,
       credentials.password);
   }
 
   signUp(credentials) {
-    return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email, credentials.password);
+    return this.afAuth.createUserWithEmailAndPassword(credentials.email, credentials.password);
   }
 
   get authenticated(): boolean {
@@ -35,7 +35,7 @@ export class AuthService {
   }
 
   signOut(): Promise<void> {
-    return this.afAuth.auth.signOut();
+    return this.afAuth.signOut();
   }
 
   signInWithGoogle(): Promise<any> {
@@ -44,19 +44,19 @@ export class AuthService {
   }
 
   private oauthSignIn(provider: AuthProvider) {
-    if (!(<any>window).cordova) {
-      return this.afAuth.auth.signInWithPopup(provider);
+    if (!( window as any).cordova) {
+      return this.afAuth.signInWithPopup(provider);
     } else {
-      return this.afAuth.auth.signInWithRedirect(provider)
+      return this.afAuth.signInWithRedirect(provider)
         .then(() => {
-          return this.afAuth.auth.getRedirectResult().then(result => {
+          return this.afAuth.getRedirectResult().then(result => {
             // This gives you a Google Access Token.
             // You can use it to access the Google API.
-            let token = result.credential.providerId;
+            const token = result.credential.providerId;
             // The signed-in user info.
-            let user = result.user;
+            const user = result.user;
             console.log(token, user);
-          }).catch(function (error) {
+          }).catch(function(error) {
             // Handle Errors here.
             alert(error.message);
           });
